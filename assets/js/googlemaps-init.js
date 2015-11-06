148911c27411dcd381e545305c75ad58b64e52d5
Wesley Burden <wesley@nebulodesign.com>
2015-11-06 10:22:11 +0000

    update map location for Maidstone

--- assets/js/googlemaps-init.js
+++ assets/js/googlemaps-init.js
@@ -1,6 +1,6 @@
 var bittersMap = (function () {
-  var myLatlng = new google.maps.LatLng(51.0856003,1.0350555),
-      mapCenter = new google.maps.LatLng(51.0856003,1.0350555),
+  var myLatlng = new google.maps.LatLng(51.2744656,0.5195365),
+      mapCenter = new google.maps.LatLng(51.2744656,0.5195365),
       mapCanvas = document.getElementById('map_canvas'),
       mapOptions = {
         center: mapCenter,
@@ -17,7 +17,7 @@ var bittersMap = (function () {
           '</div>'+
           '<h1 id="firstHeading" class="firstHeading">codeHarbour</h1>'+
           '<div id="bodyContent"'+
-          '<p>Holiday Extras</p>'+
+          '<p>The Art Cafe</p>'+
           '</div>'+
           '</div>',
       infowindow = new google.maps.InfoWindow({
@@ -27,7 +27,7 @@ var bittersMap = (function () {
       marker = new google.maps.Marker({
         position: myLatlng,
         map: map,
-        title: 'thoughtbot (Sweden)'
+        title: 'codeHarbour'
       });
 
   return {
